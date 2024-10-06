432fc5260e7259295b1e69907c8249a48a41482c
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-10-06 03:20:47 +0000

    Project with Modal

--- src/js/component/Contact.js
+++ src/js/component/Contact.js
@@ -25,10 +25,31 @@ const Contact = ({ id, name, phone, email, address }) => {
                                 <i className="fa-solid fa-pen-to-square"></i>
                             </Link>
 
-                            <button onClick={() => actions.deleteContact(id)} className="btn btn-white d-flex align-items-top" type="submit">
+                            {/* <button onClick={() => actions.deleteContact(id)} className="btn btn-white d-flex align-items-top" type="submit">
+                                <i className="fas fa-trash-can"></i>
+                            </button> */}
+
+                            {/*Button de Bootstrap */}
+                            <button type="button" className="btn btn-white d-flex align-items-top" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <i className="fas fa-trash-can"></i>
                             </button>
-
+                            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div className="modal-dialog">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h1 className="modal-title fs-5" id="exampleModalLabel">Are you sure!</h1>
+                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div className="modal-body">
+                                            If you delete this thing the entire universe will go down!
+                                        </div>
+                                        <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Oh no!</button>
+                                            <button onClick={() => actions.deleteContact(id)} type="submit" className="btn btn-primary">Yes, baby!</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
